4959a3c7d2b5d5ca5613264d4838ef69c684d2a5
TimothyJNeale <sixhillsai@icloud.com>
2024-06-04 15:42:26 +0100

    Create a custom WTForm widget

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,5 +1,6 @@
+from typing import Any
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField, URLField
 from wtforms.validators import InputRequired, NumberRange
 
 class MovieForm(FlaskForm):
@@ -8,3 +9,26 @@ class MovieForm(FlaskForm):
     year = IntegerField("Year", validators=[InputRequired(), 
                                             NumberRange(min=1878, message="Year must be greater than 1878")])
     submit = SubmitField("Add Movie")
+
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist: list[Any]):
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringField("Series")
+    tags = StringListField("Tags")
+    description = TextAreaField("Description")
+    video_link = URLField("Video Link")
+
+    submit = SubmitField("Submit")
