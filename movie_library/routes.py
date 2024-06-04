1cb4517ad42fc9a2250bcade9d2d4da809bb4043
TimothyJNeale <sixhillsai@icloud.com>
2024-06-04 14:24:25 +0100

    Change the movie rating value

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -47,7 +47,12 @@ def movie(_id: str):
     movie = Movie(**movie_data)
     return render_template("movie_details.html", movie=movie)
 
+@pages.get("/movie/<string:_id>/rate")
+def rate_movie(_id: str):
+    rating = int(request.args.get("rating"))
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
 
+    return redirect(url_for(".movie", _id=_id))
 
 @pages.get("/toggle-theme")
 def toggle_theme():
